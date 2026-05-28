73c81f6e5b434397c7013737c707f3b2b2ede4b0
omaxzy <suskoila052@gmail.com>
2026-05-29 01:53:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -503,6 +503,7 @@ def export_orders():
 
 if __name__ == '__main__':
     with app.app_context():
+        db.drop_all()
         db.create_all()
         create_admin()
     port = int(os.environ.get('PORT', 10000))
